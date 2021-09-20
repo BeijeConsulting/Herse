655244f278af1247d5dae98263df36e78207cd8c
ivfly <iv0fly@gmail.com>
2021-09-20 16:45:46 +0200

    write XML

--- src/it/beije/herse/file/RubricaXML.java
+++ src/it/beije/herse/file/RubricaXML.java
@@ -1,13 +1,21 @@
 package it.beije.herse.file;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.crypto.dsig.TransformException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -27,7 +35,7 @@ public class RubricaXML {
 		return childElements;
 	}
 
-	public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+	public static void readXML() throws ParserConfigurationException, IOException, SAXException {
 		File f = new File("/temp/rubrica.xml");
 		System.out.println("exists ? " + f.exists());
 		
@@ -56,6 +64,54 @@ public class RubricaXML {
 			System.out.println();
 		}
 		
+	}
+	
+	public static void main(String args[]) throws TransformerConfigurationException, ParserConfigurationException,
+	TransformerException, IOException, SAXException {
+//		FileWriter writer = new FileWriter("/temp/prova.xml");
+//		writer.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+//		writer.append("<rubrica><nome>Mario</nome></rubrica>");
+//		writer.flush();
+//		writer.close();
+
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		
+		Document document = documentBuilder.newDocument();
+		
+		Element documentElement = document.createElement("contatti");
+		document.appendChild(documentElement);
+		
+		Element contatto1 = document.createElement("contatto");
+		Element nome1 = document.createElement("nome");
+		nome1.setTextContent("Chiara");
+		Element cognome1 = document.createElement("cognome");
+		cognome1.setTextContent("Collura");
+		Element telefono1 = document.createElement("telefono");
+		telefono1.setTextContent("3331234567");
+		
+		contatto1.appendChild(nome1);
+		contatto1.appendChild(cognome1);
+		contatto1.appendChild(telefono1);
+		
+		//... altri contatti
+		
+		documentElement.appendChild(contatto1);
+		
+		// write the content into xml file
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		DOMSource source = new DOMSource(document);
+		
+		StreamResult result = new StreamResult(new File("/temp/new_rubrica.xml"));
+
+		// Output to console for testing
+		StreamResult syso = new StreamResult(System.out);
+
+		transformer.transform(source, result);
+		transformer.transform(source, syso);
+
+		System.out.println("File saved!");		
 	}
 
 }
