30b2186b17b584bdb3401d520fbc3159f81c1790
savino11 <savinosgarro3@gmail.com>
2021-09-28 14:26:56 +0200

    update maven

--- src/it/beije/herse/file/Contatto.java
+++ src/it/beije/herse/file/Contatto.java
@@ -16,6 +16,10 @@ public class Contatto {
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	public String getNome() {
+		return nome;
+	}
 
 	public void setNome(String nome) {
 		this.nome = nome;
