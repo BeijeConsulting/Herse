5f1d64975c374da10d4527983df1cf08a337e654
VecchiaNiccolo <vecchianiccolo@gmail.com>
2021-10-05 11:33:18 +0200

    JPA
    Add PhoneContactsJPA and PhoneContManagerJPA classes

---
+++ src/it/beije/herse/db/jpa/PhoneContManagerJPA.java
@@ -0,0 +1,368 @@
+package it.beije.herse.db.jpa;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import it.beije.herse.file.Contatto;
+import it.beije.herse.file.csv.PhoneContactsCSV;
+import it.beije.herse.file.xml.PhoneContactsXML;
+
+/*
+*  	implementare un gestore di rubrica a linea di comando, con le seguenti funzionalità:
+*	X vedi lista contatti (con possibilità di ordinare per nome e cognome a scelta)
+*	X cerca contatto
+*	X inserisci nuovo contatto
+*	X modifica contatto
+*	X cancella contatto
+*	X trova contatti duplicati
+*	X unisci contatti duplicati
+*	X import XML/CSV
+*	X export XML/CSV
+*/
+public class PhoneContManagerJPA {
+	
+	private static Scanner s = new Scanner(System.in);
+	private static String input;
+	private static final String pathExportCSV = "/temp/file/RubricaManager/rubricaCSV.csv";
+	private static final String pathExportXML = "/temp/file/RubricaManager/rubricaXML.xml";
+	
+	private static void menu() {
+		// JPA log all'inizio
+		PhoneContactsJPA.readRubricaJPA("id", "-1");
+		
+		do {
+			System.out.println();
+			System.out.println("SELEZIONARE AZIONE:");
+			System.out.println("-> VEDERE LISTA CONTATTI");
+			System.out.println("-> CERCARE CONTATTO");
+			System.out.println("-> INSERIRE NUOVO CONTATTO");
+			System.out.println("-> MODIFICARE CONTATTO");
+			System.out.println("-> RIMUOVERE CONTATTO");
+			System.out.println("-> TROVARE CONTATTI DUPLICATI");
+			System.out.println("-> UNIRE CONTATTI DUPLICATI");
+			System.out.println("-> IMPORTARE RUBRICA");
+			System.out.println("-> ESPORTARE RUBRICA");
+			System.out.println("-> USCIRE");
+			
+			input = s.nextLine().toUpperCase();
+			switch(input) {
+			case "VEDERE LISTA CONTATTI":
+				readListaContatti();
+				break;
+			case "CERCARE CONTATTO":
+				searchContatto();
+				break;
+			case "INSERIRE NUOVO CONTATTO":
+				writeContatto();
+				break;
+			case "MODIFICARE CONTATTO":
+				modifyContatto();
+				break;
+			case "RIMUOVERE CONTATTO":
+				removeContatto();
+				break;
+			case "TROVARE CONTATTI DUPLICATI":
+				findDuplicate();
+				break;
+			case "UNIRE CONTATTI DUPLICATI":
+				removeDuplicate();
+				break;
+			case "IMPORTARE RUBRICA":
+				importXMLorCSV();
+				break;
+			case "ESPORTARE RUBRICA":
+				exportXMLorCSV();
+				break;
+			case "USCIRE":
+				return ;
+			default:
+				System.out.println("INSERIRE UNA RISPOSTA VALIDA");
+				break;
+			}
+		}while(!input.equalsIgnoreCase("USCIRE"));
+	}
+	
+	public static void exportXMLorCSV() {
+		System.out.println("SELEZIONARE TIPO RUBRICA:");
+		System.out.println("-> CSV");
+		System.out.println("-> XML");
+		boolean notValidInput = false;
+		do {
+			notValidInput = false;
+			input = s.nextLine().toUpperCase();
+			switch(input) {
+			case "CSV":
+				System.out.println("INSERIRE SEPARATORE: ");
+				String separator = s.nextLine();
+				PhoneContactsCSV.writeRubricaCSV(PhoneContactsJPA.readRubricaJPA(), pathExportCSV, separator);
+				System.out.println("CONTATTI ESPORTATI");
+				break;
+			case "XML":
+				PhoneContactsXML.writeRubricaXML(PhoneContactsJPA.readRubricaJPA(), pathExportXML);
+				System.out.println("CONTATTI ESPORTATI");
+				break;
+			default:
+				notValidInput = true;
+				System.out.println("INSERIRE UNA RISPOSTA VALIDA");
+				break;
+			}
+		}while(notValidInput);
+	}
+	
+	public static void importXMLorCSV() {
+		boolean notValidInput = false;
+		do {
+			System.out.println("INSERIRE PATH DEL FILE: ");
+			notValidInput = false;
+			String path = s.nextLine().toLowerCase();
+			
+			if(path.endsWith("csv") && new File(path).exists()) {
+				System.out.println("INSERIRE SEPARATORE: ");
+				String separator = s.nextLine();
+				PhoneContactsJPA.insertRubricaJPA(PhoneContactsCSV.readRubricaCSV(path, separator));
+				System.out.println("CONTATTI IMPORTATI");
+			}
+			else if(path.endsWith("xml") && new File(path).exists()) {
+				PhoneContactsJPA.insertRubricaJPA(PhoneContactsXML.readRubricaXML(path));
+				System.out.println("CONTATTI IMPORTATI");
+			}
+			else{
+				notValidInput = true;
+				System.out.println("INSERIRE UNA RISPOSTA VALIDA");
+			}
+		}while(notValidInput);
+	}
+
+	private static void removeDuplicate() {
+		PhoneContactsJPA.removeDuplicateJPA();
+		System.out.println("DUPLICATI UNITI");
+	}
+
+	private static void findDuplicate() {
+		System.out.println();
+		System.out.println("DUPLICATI: ");
+		PhoneContactsJPA.printRubrica(PhoneContactsJPA.findDuplicateJPA());
+	}
+
+	private static void removeContatto() {
+		boolean notValidInput = false;
+		String where = null;
+		do {
+			System.out.println();
+			System.out.println("SPECIFICARE CAMPO PER LA RIMOZIONE: ");
+			
+			notValidInput = false;
+			input = s.nextLine().toUpperCase();
+			switch(input) {
+			case "ID":
+				where = "id";
+				break;
+			case "NOME":
+				where = "nome";
+				break;
+			case "COGNOME":
+				where = "cognome";
+				break;
+			case "TELEFONO":
+				where = "telefono";
+				break;
+			case "EMAIL":
+				where = "email";
+				break;
+			case "NOTE":
+				where = "note";
+				break;
+			default:
+				notValidInput = true;
+				System.out.println("INSERIRE UNA RISPOSTA VALIDA");
+				break;
+			}
+		}while(notValidInput);
+		System.out.println();
+		System.out.println("SPECIFICARE VALORE DEL CAMPO: ");
+		String val = s.nextLine();
+		
+		PhoneContactsJPA.deleteRubricaJPA(where, val);
+		
+		System.out.println("CONTATTO RIMOSSO");
+	}
+
+	private static void modifyContatto() {
+		boolean notValidInput = false;
+		String where = null;
+		do {
+			System.out.println();
+			System.out.println("SPECIFICARE CAMPO PER LA RICERCA: ");
+			
+			notValidInput = false;
+			input = s.nextLine().toUpperCase();
+			switch(input) {
+			case "ID":
+				where = "id";
+				break;
+			case "NOME":
+				where = "nome";
+				break;
+			case "COGNOME":
+				where = "cognome";
+				break;
+			case "TELEFONO":
+				where = "telefono";
+				break;
+			case "EMAIL":
+				where = "email";
+				break;
+			case "NOTE":
+				where = "note";
+				break;
+			default:
+				notValidInput = true;
+				System.out.println("INSERIRE UNA RISPOSTA VALIDA");
+				break;
+			}
+		}while(notValidInput);
+		System.out.println();
+		System.out.println("SPECIFICARE VALORE DEL CAMPO: ");
+		String whereVal = s.nextLine();
+		
+		notValidInput = false;
+		String attr = null;
+		do {
+			System.out.println();
+			System.out.println("INSERIRE CAMPO DA MODIFICARE: ");
+			
+			notValidInput = false;
+			input = s.nextLine().toUpperCase();
+			switch(input) {
+			case "ID":
+				attr = "id";
+				break;
+			case "NOME":
+				attr = "nome";
+				break;
+			case "COGNOME":
+				attr = "cognome";
+				break;
+			case "TELEFONO":
+				attr = "telefono";
+				break;
+			case "EMAIL":
+				attr = "email";
+				break;
+			case "NOTE":
+				attr = "note";
+				break;
+			default:
+				notValidInput = true;
+				System.out.println("INSERIRE UNA RISPOSTA VALIDA");
+				break;
+			}
+		}while(notValidInput);
+		System.out.println();
+		System.out.println("INSERIRE NUOVO VALORE DEL CAMPO: ");
+		String attrVal = s.nextLine();
+		
+		PhoneContactsJPA.modifyRubricaJPA(attr, attrVal, where, whereVal);
+		
+		System.out.println("CONTATTO MODIFICATO");
+	}
+
+	private static void writeContatto() {
+		List<Contatto> contatto = new ArrayList<>();
+		Contatto nuovoContatto = new Contatto();
+		contatto.add(nuovoContatto);
+		
+		System.out.println();
+		System.out.println("INSERIRE NOME: ");
+		nuovoContatto.setNome(s.nextLine());
+		System.out.println("INSERIRE COGNOME: ");
+		nuovoContatto.setCognome(s.nextLine());
+		System.out.println("INSERIRE TELEFONO: ");
+		nuovoContatto.setTelefono(s.nextLine());
+		System.out.println("INSERIRE EMAIL: ");
+		nuovoContatto.setEmail(s.nextLine());
+		System.out.println("INSERIRE NOTE: ");
+		nuovoContatto.setNote(s.nextLine());
+		
+		PhoneContactsJPA.insertRubricaJPA(contatto);
+		
+		System.out.println("CONTATTO INSERITO");
+	}
+
+	private static void searchContatto() {
+		boolean notValidInput = false;
+		String where = null;
+		do {
+			System.out.println();
+			System.out.println("SPECIFICARE CAMPO PER LA RICERCA: ");
+			
+			notValidInput = false;
+			input = s.nextLine().toUpperCase();
+			switch(input) {
+			case "ID":
+				where = "id";
+				break;
+			case "NOME":
+				where = "nome";
+				break;
+			case "COGNOME":
+				where = "cognome";
+				break;
+			case "TELEFONO":
+				where = "telefono";
+				break;
+			case "EMAIL":
+				where = "email";
+				break;
+			case "NOTE":
+				where = "note";
+				break;
+			default:
+				notValidInput = true;
+				System.out.println("INSERIRE UNA RISPOSTA VALIDA");
+				break;
+			}
+		}while(notValidInput);
+		System.out.println();
+		System.out.println("SPECIFICARE VALORE DEL CAMPO: ");
+		String val = s.nextLine();
+		
+		PhoneContactsJPA.printRubrica(PhoneContactsJPA.readRubricaJPA(where, val));
+	}
+
+	private static void readListaContatti() {
+		boolean notValidInput = false;
+		do {
+			System.out.println();
+			System.out.println("ORDINARE PER: ");
+			System.out.println("-> NOME");
+			System.out.println("-> COGNOME");
+			System.out.println("-> NON ORDINARE");
+			
+			notValidInput = false;
+			input = s.nextLine().toUpperCase();
+			switch(input) {
+			case "NOME":
+				PhoneContactsJPA.printRubrica(PhoneContactsJPA.readRubricaJPA("nome"));
+				break;
+			case "COGNOME":
+				PhoneContactsJPA.printRubrica(PhoneContactsJPA.readRubricaJPA("cognome"));
+				break;
+			case "NON ORDINARE":
+				PhoneContactsJPA.printRubrica(PhoneContactsJPA.readRubricaJPA());
+				break;
+			default:
+				notValidInput = true;
+				System.out.println("INSERIRE UNA RISPOSTA VALIDA");
+				break;
+			}
+		}while(notValidInput);
+	}
+
+	public static void main(String[] args) {
+		menu();
+	}
+
+}
---
+++ src/it/beije/herse/db/jpa/PhoneContactsJPA.java
@@ -0,0 +1,203 @@
+package it.beije.herse.db.jpa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import it.beije.herse.file.Contatto;
+
+public class PhoneContactsJPA {
+	
+	private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Herse");
+	private static EntityManager entityManager;
+	private static EntityTransaction transaction;
+
+	private static final List<String> contatto = Arrays.asList("ID", "NOME", "COGNOME", "TELEFONO", "EMAIL", "NOTE");
+
+	public static List<Contatto> readRubricaJPA(){
+		List<Contatto> rubrica = new ArrayList<>();
+		
+		entityManager = entityManagerFactory.createEntityManager();
+		Query query = entityManager.createQuery("SELECT c FROM Contatto as c");
+		
+		rubrica = query.getResultList(); 
+		entityManager.close();
+		
+		return rubrica;
+	}
+	
+	public static List<Contatto> readRubricaJPA(String where, String val){
+		List<Contatto> rubrica = new ArrayList<>();
+		
+		entityManager = entityManagerFactory.createEntityManager();
+		Query query = null;
+		
+		try {
+			Integer id=null;
+			if(!contatto.contains(where.toUpperCase())) throw new Exception();
+			where = where.toLowerCase();
+			if(where.equalsIgnoreCase("id")) {
+				id = Integer.parseInt(val);
+				query = entityManager.createQuery("SELECT c FROM Contatto as c WHERE "+where+"="+"'"+id+"'");
+			}
+			else query = entityManager.createQuery("SELECT c FROM Contatto as c WHERE "+where+"="+"'"+val+"'");
+			
+			rubrica = query.getResultList(); 
+		}
+		catch(Exception ex){
+			System.out.println("NESSUNA COLONNA VALIDA");
+			rubrica.add(new Contatto("ERRORE", "ERRORE", "ERRORE", "ERRORE", "ERRORE"));
+		}
+		finally {
+			entityManager.close();
+		}
+		
+		return rubrica;
+	}
+	
+	public static List<Contatto> readRubricaJPA(String orderBy){
+		List<Contatto> rubrica = new ArrayList<>();
+		
+		entityManager = entityManagerFactory.createEntityManager();
+		Query query = null;
+		
+		try {
+			if(!contatto.contains(orderBy.toUpperCase())) throw new Exception();
+			orderBy = orderBy.toLowerCase();
+			query = entityManager.createQuery("SELECT c FROM Contatto as c ORDER BY "+orderBy);
+			rubrica = query.getResultList(); 
+			
+		} 
+		catch (Exception e) {
+			System.out.println("NESSUNA COLONNA VALIDA");
+			rubrica.add(new Contatto("ERRORE", "ERRORE", "ERRORE", "ERRORE", "ERRORE"));
+		}
+		finally {
+			entityManager.close();
+		}
+		
+		return rubrica;
+	}
+	
+	public static void printRubrica(List<Contatto> rubrica) {
+		for (Contatto c : rubrica) {
+			System.out.println(c);
+		}
+		System.out.println();
+	}
+	
+	public static void insertRubricaJPA(List<Contatto> rubrica) {
+		entityManager = entityManagerFactory.createEntityManager();
+		transaction = entityManager.getTransaction();
+		transaction.begin();
+		
+		for (Contatto c : rubrica) entityManager.persist(c);
+		
+		transaction.commit();
+		entityManager.close();
+	}
+	
+	public static void deleteRubricaJPA(String where, String val) {
+		for(Contatto c : readRubricaJPA(where, val)) {
+			entityManager = entityManagerFactory.createEntityManager();
+			transaction = entityManager.getTransaction();
+			transaction.begin();
+			
+			entityManager.remove(c);
+			
+			transaction.commit();
+			entityManager.close();
+		}
+	}
+	
+	public static void modifyRubricaJPA(String attr, String attrVal, String where, String whereVal) {
+		List<Contatto> rubrica = new ArrayList<>();
+		
+		entityManager = entityManagerFactory.createEntityManager();
+		transaction = entityManager.getTransaction();
+		transaction.begin();
+		Query query = null;
+		
+		try {
+			Integer id=null;
+			if(!contatto.contains(where.toUpperCase())) throw new Exception();
+			where = where.toLowerCase();
+			if(where.equalsIgnoreCase("id")) {
+				id = Integer.parseInt(whereVal);
+				query = entityManager.createQuery("SELECT c FROM Contatto as c WHERE "+where+"="+"'"+id+"'");
+			}
+			else query = entityManager.createQuery("SELECT c FROM Contatto as c WHERE "+where+"="+"'"+whereVal+"'");
+			
+			rubrica = query.getResultList();
+			
+			for(Contatto c : rubrica) {
+				switch(attr.toUpperCase()) {
+				case "NOME":
+					c.setNome(attrVal);
+					break;
+				case "COGNOME":
+					c.setCognome(attrVal);
+					break;
+				case "EMAIL":
+					c.setEmail(attrVal);
+					break;
+				case "TELEFONO":
+					c.setTelefono(attrVal);
+					break;
+				case "NOTE":
+					c.setNote(attrVal);
+					break;
+				}
+				entityManager.persist(c);
+			}
+		}
+		catch(Exception ex){
+			System.out.println("NESSUNA COLONNA VALIDA");
+			rubrica.add(new Contatto("ERRORE", "ERRORE", "ERRORE", "ERRORE", "ERRORE"));
+		}
+		finally {
+			transaction.commit();
+			entityManager.close();
+		}
+	}
+	
+	public static void removeDuplicateJPA() {
+		for(Contatto c : findDuplicateJPA()) {
+			entityManager = entityManagerFactory.createEntityManager();
+			transaction = entityManager.getTransaction();
+			transaction.begin();
+			
+			entityManager.remove(c);
+			
+			transaction.commit();
+			entityManager.close();
+		}
+	}
+	
+	public static List<Contatto> findDuplicateJPA(){
+		List<Contatto> duplicates = new ArrayList<>();
+		
+		entityManager = entityManagerFactory.createEntityManager();
+		
+		String duplicateQuery = "select c from Contatto as c group by nome, cognome, telefono, email, note "
+			+"having (count(nome) > 1) and (count(cognome) > 1) and (count(telefono) > 1) and "
+				+ "(count(email) > 1) and (count(note) > 1)";
+		Query query = entityManager.createQuery(duplicateQuery);
+		
+		duplicates = query.getResultList(); 
+		entityManager.close();
+		
+		return duplicates;
+	}
+	
+	public static void main(String[] args) {
+		printRubrica(readRubricaJPA());
+	}
+
+}
