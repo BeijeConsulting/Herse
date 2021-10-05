5fd252fe54f2d3c023a5a93a544a2a98324b55c9
omarelmounjid <omar.elmo@icloud.com>
2021-10-05 11:43:12 +0200

    JPA

---
+++ src/it/beije/herse/oca/DB/MetodiJPA.java
@@ -0,0 +1,14 @@
+package it.beije.herse.oca.DB;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class MetodiJPA {
+		EntityManagerFactory entytymangaerfactory=Persistence.createEntityManagerFactory("Herse");
+		EntityManager entitymanager=EntityManager.createEntityManager();
+		
+		
+	
+
+}
