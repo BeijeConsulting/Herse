21fb7245b8896766fd188497f4289dd27bbdf40c
TheMax404 <massimiliano.borgese@gmail.com>
2021-10-25 08:35:55 +0200

    Prove in vista dell'OCA

--- src/esercizi/Prova.java
+++ src/esercizi/Prova.java
@@ -1,7 +1,15 @@
 package esercizi;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public  class Prova {
-	static void stampa() {
-		System.out.println("sono nell'interfaccia");
+
+
+	 public static void main(String[] args) {
+		 List<String> list = new ArrayList<>();
+		 list.removeIf((String s)->s.isEmpty());
 	}
 }
